ba65def9c4101018175c2be4a5601af1f9207c05
aschonely83 <aschonely@gmail.com>
2021-01-05 20:06:31 -0500

    built index and show actions

--- app/controllers/pallets_controller.rb
+++ app/controllers/pallets_controller.rb
@@ -1,2 +1,14 @@
 class PalletsController < ApplicationController
+    def index
+      pallets = Pallet.all
+      render json: PalletSerializer.new(pallets)    
+    end
+    
+    def show
+      pallet = Pallet.find_by(id: params[:id])
+      options = {
+        include: [:retailer]    
+      }
+      render json: PalletSerializer.new(pallet, options)    
+    end
 end
