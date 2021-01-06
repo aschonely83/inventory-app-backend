56907cbfd41443e4716ddf5651f97e288f9bc9f1
aschonely83 <aschonely@gmail.com>
2021-01-05 19:30:58 -0500

    updated database

--- db/migrate/20210105225207_create_pallets.rb
+++ db/migrate/20210105225207_create_pallets.rb
@@ -1,9 +1,10 @@
-class CreatePallets < ActiveRecord::Migration[6.0]
+class CreatePallet < ActiveRecord::Migration[6.0]
   def change
-    create_table :pallets do |t|
+    create_table :pallet do |t|
       t.integer :retailer_id
       t.string :boxes
       t.references :retailer, null: false, foreign_key: true
+      t.index ["retailer_id"], name: "index_pallets_on_retailer_id"
 
       t.timestamps
     end
