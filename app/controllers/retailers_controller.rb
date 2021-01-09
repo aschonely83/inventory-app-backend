8416eb73359896f646611b1ab0d5ba5ee2bee760
aschonely83 <aschonely@gmail.com>
2021-01-09 12:04:52 -0500

    built index and show actions

--- app/controllers/retailers_controller.rb
+++ app/controllers/retailers_controller.rb
@@ -1,2 +1,11 @@
 class RetailersController < ApplicationController
+    def index
+        retailers = Retailer.all
+        render json: RetailerSerializer.new(retailers)    
+    end
+      
+    def show
+      retailer = Retailer.find_by(id: params[:id])
+      render json: RetailerSerializer.new(retailer)    
+    end
 end
