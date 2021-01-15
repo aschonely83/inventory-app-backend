ad895329c94dbe01c1a6b76bbb9ffebc461d27c9
aschonely83 <aschonely@gmail.com>
2021-01-15 15:33:32 -0500

    update index and create methods

--- app/controllers/retailers_controller.rb
+++ app/controllers/retailers_controller.rb
@@ -1,24 +1,31 @@
 class RetailersController < ApplicationController
     def index
-        retailers = Retailer.all
-        render json: RetailerSerializer.new(retailers)    
+        @retailers = Retailer.all
+        render json: RetailerSerializer.new(@retailers).serializable_hash[:data].map {|hash| hash[:attributes] }    
     end
       
     def show
-      retailer = Retailer.find_by(id: params[:id])
-      render json: RetailerSerializer.new(retailer)    
+      @retailer = Retailer.find_by(id: params[:id])
+      render json: RetailerSerializer.new(@retailer)   
     end
 
     def create
-      retailer = Retailer.create(retailer_params)
-      if retailer.save
-        render json: RetailerSerializer.new(retailer)
+      @retailer = Retailer.new(retailer_params)
+      
+      if @retailer.save
+        render json: RetailerSerializer.new(@retailer).serializable_hash[:data][:attributes], status: :created, location: @retailer
+      else
+        render json: @retailer.errors.full_messages.to_sentence, status: :unprocessable_entity  
       end
     end
 
     private
 
+    def set_retailer
+      @retailer = Retailer.find(params[:id])
+    end
+
     def retailer_params
-      params.permit(:name)
+      params.require(:retailer).permit(:name)
     end
 end
