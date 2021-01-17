9674525b377803306274ca3509cdcf6a46e55b55
aschonely83 <aschonely@gmail.com>
2021-01-17 18:02:52 -0500

    built create and update methods for pallet

--- app/controllers/pallets_controller.rb
+++ app/controllers/pallets_controller.rb
@@ -1,25 +1,43 @@
 class PalletsController < ApplicationController
+  before_action :set_pallet, only: [:show, :update, :destroy]
+
     def index
-      pallets = Pallet.all
-      render json: PalletSerializer.new(pallets)    
+      @pallets = Pallet.all
+      
+      render json: @pallets    
     end
     
     def show
-      pallet = Pallet.find_by(id: params[:id])
-      options = {
-        include: [:retailer]    
-      }
-      render json: PalletSerializer.new(pallet, options)    
+     render json: @pallets
     end
 
     def create
-      pallet = Pallet.create(pallet_params)
-      if pallet.save
-        render json: PalletSerializer.new(pallet)   
+      @pallet = Pallet.new(pallet_params)
+      
+      if @pallet.save
+        render json: PalletSerializer.new(@pallet).serializable_hash[:data][:attributes], status: :created, location: @pallet
+      else
+        render json: @pallet.errors.full_messages.to_sentence, status: :unprocessable_entity     
       end        
     end
 
+    def update
+      if @pallet.update(pallet_params)
+        render json: @pallet
+      else 
+        render json: @pallet.errors, status: :unprocessable_entity   
+      end
+    end
+
+    def destroy
+      @pallet.destroy
+    end
+
     private
+
+    def set_pallet
+      @pallet = Pallet.find(params[:id])
+    end
 
     def pallet_params
       params.require(:retailer).permit(:boxes, :retailer_id)
